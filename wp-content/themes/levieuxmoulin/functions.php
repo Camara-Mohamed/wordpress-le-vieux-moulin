b05549b1340735c220082ee0a820a1f3778304a9
Camara-Mohamed <mohamed.camara1@student.hepl.be>
2025-06-12 07:30:49 +0200

    feat: build header and navigation menu

--- wp-content/themes/levieuxmoulin/functions.php
+++ wp-content/themes/levieuxmoulin/functions.php
@@ -12,3 +12,15 @@ require_once get_template_directory() . '/inc/setup.php';
 require_once get_template_directory() . '/inc/assets.php';
 require_once get_template_directory().'/inc/post-types.php';
 require_once get_template_directory() . '/inc/navigation.php';
+
+// Page d'option
+if(function_exists('acf_add_options_page')) {
+    acf_add_options_page(array(
+        'page_title' => 'Informations générales',
+        'menu_title' => 'Informations générales',
+        'menu_slug' => 'some-informations',
+        'capability' => 'edit_posts',
+        'icon_url'      => 'dashicons-info',
+        'redirect' => false
+    ));
+}
