82994f7b095e5c60a3cfb1a3d4417fdeea6c20a9
Camara-Mohamed <mohamed.camara1@student.hepl.be>
2025-06-12 18:18:45 +0200

    feat: build donations page with ACF field

--- wp-content/themes/levieuxmoulin/Template-dons.php
+++ wp-content/themes/levieuxmoulin/Template-dons.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template Name: Me Contacter
+ * Template Name: Dons
  */
 ?>
 
@@ -9,6 +9,47 @@
 <main id="main-content" class="main__content">
     <?php get_template_part('templates/partials/hero-section') ?>
 
+    <?php if (have_rows('donation_sections')) : ?>
+        <div class="donation">
+            <?php while (have_rows('donation_sections')) : the_row(); ?>
+                <section class="donation">
+                        <h2 class="donation__title"><?= get_sub_field('title'); ?></h2>
+
+                        <div class="donation__content">
+                            <?php $image = get_sub_field('image'); ?>
+                            <?php if ($image) : ?>
+                                <div class="donation__image">
+                                    <?= wp_get_attachment_image($image, 'medium'); ?>
+                                </div>
+                            <?php endif; ?>
+
+                            <p class="donation__description">
+                                <?= get_sub_field('description'); ?>
+                            </p>
+                        </div>
+                </section>
+            <?php endwhile; ?>
+        </div>
+    <?php endif; ?>
+
+    <?php if (have_rows('realisation')) : ?>
+        <?php while (have_rows('realisation')) : the_row(); ?>
+            <section class="realisation">
+                    <h2 class="realisation__title"><?= get_sub_field('title'); ?></h2>
+
+                    <div class="realisation__grid">
+                        <?php while (have_rows('boxes')) : the_row(); ?>
+                            <div class=box">
+                                <h3 class="box__title"><?= get_sub_field('title'); ?></h3>
+                                <p class="box__description">
+                                    <?= get_sub_field('description'); ?>
+                                </p>
+                            </div>
+                        <?php endwhile; ?>
+                    </div>
+            </section>
+        <?php endwhile; ?>
+    <?php endif; ?>
 </main>
 
 <?php get_footer() ?>
