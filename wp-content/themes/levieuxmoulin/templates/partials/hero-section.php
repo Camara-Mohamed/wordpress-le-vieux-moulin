cce04fac426f801ec5aef1acb0592a8a65d4c9be
Camara-Mohamed <mohamed.camara1@student.hepl.be>
2025-06-14 12:53:53 +0200

    feat: style hero section

--- wp-content/themes/levieuxmoulin/templates/partials/hero-section.php
+++ wp-content/themes/levieuxmoulin/templates/partials/hero-section.php
@@ -1,10 +1,4 @@
 <section class="hero">
-    <?php if(get_field('hero_image')): ?>
-        <div class="hero__image">
-            <?= wp_get_attachment_image(get_field('hero_image'), 'full'); ?>
-        </div>
-    <?php endif; ?>
-
     <div class="hero__content">
             <?php if(get_field('hero_title')): ?>
                 <h2 class="hero__title"><?= get_field('hero_title'); ?></h2>
@@ -18,11 +12,16 @@
                 <div class="hero__buttons">
                     <?php foreach(get_field('hero_buttons') as $button): ?>
                         <a href="<?= $button['button_url']; ?>"
-                           class="button button--<?= $button['button_style']; ?>">
+                           class="button button__<?= $button['button_style']; ?>">
                             <?= $button['button_text']; ?>
                         </a>
                     <?php endforeach; ?>
                 </div>
             <?php endif; ?>
     </div>
+    <?php if(get_field('hero_image')): ?>
+        <div class="hero__image">
+            <?= wp_get_attachment_image(get_field('hero_image'), 'full'); ?>
+        </div>
+    <?php endif; ?>
 </section>
